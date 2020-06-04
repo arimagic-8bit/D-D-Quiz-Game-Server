664b3790f6bebdb40b3843a2e6aef3ab4fab4ee2
arimagic-8bit <baggerman1994@gmail.com>
2020-06-04 09:20:42 +0200

    All questions added on seed file

--- bin/seed-question.js
+++ bin/seed-question.js
@@ -39,7 +39,75 @@ const question = [
   {
     question: "Can you use a shield with Mage Armor spell?",
     picture: "",
-    answers: ["", "", ""],
+    answers: [
+      "Only with a light shield or buckler shield",
+      "Yes, Mage Armor spell works with a shield",
+      "Nope, they do not stack",
+    ],
+    correctAnswer: 1,
+    points: 10,
+  },
+  {
+    question:
+      "A monter is immune to damage from nonmagical bludgeoning weapons. Does he still take damage from falling?",
+    picture: "",
+    answers: [
+      "Yes, but has resistance to damage",
+      "No, fall is a bludgeoning damage",
+      "Yes, fall is not a weapon",
+    ],
+    correctAnswer: 2,
+    points: 10,
+  },
+  {
+    question:
+      "If a 5th level wizard casts a Fireball during surprise, do the enemies gest disavantage on their saving throw?",
+    picture: "",
+    answers: ["No", "Only if wizard has with War Caster feat", "Yes"],
+    correctAnswer: 0,
+    points: 10,
+  },
+  {
+    question: "Is a 1 on an ability check an automatic failure?",
+    picture: "",
+    answers: [
+      "Yes",
+      "No, is not an automatic failure",
+      "Yes and roll a d10 on Fumble table",
+    ],
+    correctAnswer: 1,
+    points: 10,
+  },
+  {
+    question: "Can a rogue get sneak attack damage against undead?",
+    picture: "",
+    answers: [
+      "No, undeads have resistance to sneak attack",
+      "Yep, sneak attack works againts undead",
+      "Only if you use a bludgeoning weapon",
+    ],
+    correctAnswer: 1,
+    points: 10,
+  },
+  {
+    question: "If you have a creature between you and the target...",
+    picture: "",
+    answers: [
+      "Target has cover +4 bonus to AC",
+      "Target has half-cover +2 bonus to AC",
+      "You have disavantage",
+    ],
+    correctAnswer: 1,
+    points: 10,
+  },
+  {
+    question: "Can you make an attack action from prone condition?",
+    picture: "",
+    answers: [
+      "No, you must stand up (half movement) and attack",
+      "Yes, but you have disadvantage on attack rolls",
+      "Only with a weapon with reach property",
+    ],
     correctAnswer: 1,
     points: 10,
   },
