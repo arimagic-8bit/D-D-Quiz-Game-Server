37fea86d45eaa989cc5bad75dbea355b3accdff0
arimagic-8bit <baggerman1994@gmail.com>
2020-06-11 10:24:00 +0200

    Add cors to JSON

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 //const session = require("express-session");
 //const MongoStore = require("connect-mongo")(session);
 require("dotenv").config();
@@ -30,6 +31,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    credentials: true,
+    origin: [publicDomain],
+  })
+);
 
 // ROUTER MIDDLEWARE
 app.use("/api", apiRouter);
